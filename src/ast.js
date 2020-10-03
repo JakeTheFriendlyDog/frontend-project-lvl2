af862cf254823cf49348373a9d244d3a287d69e3
nickdvdev <nickdv.dev@gmail.com>
2020-10-03 14:20:45 +0300

    fix dependabot alert

--- src/ast.js
+++ src/ast.js
@@ -15,22 +15,21 @@ export default (firstData, secondData) => {
     const onlyUniqueKeys = union(keys(first), keys(second));
 
     return onlyUniqueKeys.flatMap(((key) => {
-      if (has(first, key) && has(second, key)) {
-        if (typeof first[key] === 'object' && typeof second[key] === 'object') {
-          const children = iter(first[key], second[key], ancestry + 1, key);
-          return makeNode(key, 'unchanged', ancestry, children, parent);
-        }
-        if (first[key] === second[key]) {
-          return makeNode(key, 'unchanged', ancestry, first[key], parent);
-        }
-        return [makeNode(key, 'deleted', ancestry, first[key], parent),
-          makeNode(key, 'changed', ancestry, second[key], parent, first[key])];
+      if (!has(first, key)) {
+        return makeNode(key, 'added', ancestry, second[key], parent);
       }
-
-      if (has(first, key) && !has(second, key)) {
+      if (!has(second, key)) {
         return makeNode(key, 'deleted', ancestry, first[key], parent);
       }
-      return makeNode(key, 'changed', ancestry, second[key], parent);
+
+      if (typeof first[key] === 'object' && typeof second[key] === 'object') {
+        const children = iter(first[key], second[key], ancestry + 1, key);
+        return makeNode(key, 'unchanged', ancestry, children, parent);
+      }
+      if (first[key] === second[key]) {
+        return makeNode(key, 'unchanged', ancestry, first[key], parent);
+      }
+      return makeNode(key, 'changed', ancestry, second[key], parent, first[key]);
     }));
   };
 
